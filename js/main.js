eecd81a3f02b834e2ac35ff81709e032a43cd3d0
karpenkova123 <v.a.karpenkova@urfu.ru>
2025-12-29 21:13:34 +0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+import { renderGallery } from './full-photo.js';
+import { showSuccessMessage, showErrorMessage } from './data.js';
+import { hideModal, setOnFormSubmit } from './form.js';
+import { getData, sendData } from './fetch.js';
+import { showAlert, debounce } from './util.js';
+import { init, getFilterPictures } from './filter.js';
+import './my-photo.js';
+
+try {
+  init(await getData(), debounce(renderGallery));
+  renderGallery(getFilterPictures());
+} catch (err) {
+  showAlert(err.message);
+}
+setOnFormSubmit(async (data) => {
+  try {
+    await sendData(data);
+    hideModal();
+    showSuccessMessage();
+  } catch {
+    showErrorMessage();
+  }
+});
